a5e4059787aa95cdf67ca816ba7173f59cbac156
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-11-23 10:19:18 +0100

    Implemented YamlStrategy based on JsonStrategy

--- src/View/Strategy/YamlStrategy.php
+++ src/View/Strategy/YamlStrategy.php
@@ -1,9 +1,13 @@
 <?php
 namespace Acelaya\Yaml\View\Strategy;
 
+use Acelaya\Yaml\View\Model\YamlModel;
 use Zend\EventManager\AbstractListenerAggregate;
 use Zend\EventManager\EventManagerInterface;
+use Zend\Http\Headers;
 use Zend\View\ViewEvent;
+use Acelaya\Yaml\View\Renderer\YamlRenderer;
+use Zend\Http;
 
 /**
  * Class YamlStrategy
@@ -12,6 +16,38 @@ use Zend\View\ViewEvent;
  */
 class YamlStrategy extends AbstractListenerAggregate
 {
+    /**
+     * Character set for associated content-type
+     *
+     * @var string
+     */
+    protected $charset = 'utf-8';
+
+    /**
+     * Multibyte character sets that will trigger a binary content-transfer-encoding
+     *
+     * @var array
+     */
+    protected $multibyteCharsets = array(
+        'UTF-16',
+        'UTF-32',
+    );
+
+    /**
+     * @var YamlRenderer
+     */
+    protected $renderer;
+
+    /**
+     * Constructor
+     *
+     * @param  YamlRenderer $renderer
+     */
+    public function __construct(YamlRenderer $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+
     /**
      * Attach one or more listeners
      *
@@ -26,13 +62,72 @@ class YamlStrategy extends AbstractListenerAggregate
         $this->listeners[] = $events->attach(ViewEvent::EVENT_RESPONSE, array($this, 'injectResponse'), $priority);
     }
 
+    /**
+     * Returns the YamlRenderer if current model is a YamlModel
+     * @param ViewEvent $e
+     * @return YamlRenderer
+     */
     public function selectRenderer(ViewEvent $e)
     {
+        $model = $e->getModel();
 
+        if (! $model instanceof YamlModel) {
+            return;
+        }
+
+        // JsonModel found
+        return $this->renderer;
     }
 
     public function injectResponse(ViewEvent $e)
     {
+        $renderer = $e->getRenderer();
+        if ($renderer !== $this->renderer) {
+            // Discovered renderer is not ours; do nothing
+            return;
+        }
 
+        $result = $e->getResult();
+        if (! is_string($result)) {
+            // We don't have a string, and thus, no yaml
+            return;
+        }
+
+        // Populate response
+        /** @var Http\Response $response */
+        $response = $e->getResponse();
+        $response->setContent($result);
+        /** @var Headers $headers */
+        $headers = $response->getHeaders();
+
+        $contentType = sprintf('application/x-yaml; charset=%s', $this->charset);
+        $headers->addHeaderLine('content-type', $contentType);
+
+        // Add content-transfer-encoding header in charset is multibyte
+        if (in_array(strtoupper($this->charset), $this->multibyteCharsets)) {
+            $headers->addHeaderLine('content-transfer-encoding', 'BINARY');
+        }
+    }
+
+    /**
+     * Set the content-type character set
+     *
+     * @param  string $charset
+     * @return $this
+     */
+    public function setCharset($charset)
+    {
+        $this->charset = (string) $charset;
+        return $this;
+    }
+
+    /**
+     * Retrieve the current character set
+     *
+     * @return string
+     */
+    public function getCharset()
+    {
+        return $this->charset;
     }
 }
