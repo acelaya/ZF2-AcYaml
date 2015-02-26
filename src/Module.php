6d4dc9fb4ab3d3bb48ed51fd0cb43f199057afed
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-02-26 10:56:43 +0700

    update autoloader : rely on composer psr-4

--- src/Module.php
+++ src/Module.php
@@ -2,7 +2,6 @@
 namespace Acelaya\Yaml;
 
 use Zend\Loader\StandardAutoloader;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
 /**
@@ -10,22 +9,8 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
  * @author Alejandro Celaya Alastrué
  * @link http://www.alejandrocelaya.com
  */
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+class Module implements ConfigProviderInterface
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/../autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                StandardAutoloader::LOAD_NS => array(
-                    __NAMESPACE__ => __DIR__,
-                ),
-            ),
-        );
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
